56a1e20691dbbbacd0e9dfcecd84cd41aae2bdd6
dkrishna11 <116519809+dkrishna11@users.noreply.github.com>
2023-06-13 17:48:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let li=document.getElementByTagName("li");
+li.addEventListner("load", functio(){
+	for(let i=0;i<li.length;i++){
+	if(li[i].id==="level")
+		alert("The level of the element is: "+i);
+}
+})
